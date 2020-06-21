63fcd376add2e38f05cb9cce6a0181d1db40f569
llguedes <me@lucasguedes.com.br>
2020-06-21 20:14:05 -0300

    Adição

--- Python/apis/bacen/bacenAPI.py
+++ Python/apis/bacen/bacenAPI.py
@@ -1,21 +1,30 @@
+#%%
 import pandas as pd
 import sys; sys.path
 import seaborn as sns; sns.set()
-import matploitlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 import matplotlib
 matplotlib.rcParams['figure.figsize'] = (16,8)
 
-cod_bcb = input("Entre com o codigo: ")
+cod_bcb = input("Entre com o codigo da serie: ")
+cod = int(cod_bcb)
 
-def consulta_bc(cod_bcb):
-    url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json'.format(cod_bcb)
+def consulta_bc(cod):
+    url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json'.format(cod)
     df = pd.read_json(url)
     df['data'] = pd.to_datetime(df['data'], dayfirst=True)
-    df.set_index('data', implace=True)
+    df.set_index('data', inplace=True)
     return df
 
+ipca = consulta_bc(cod)
+plt.plot(ipca)
+plt.show()
 
 
 
 
+
+
+
+# %%
